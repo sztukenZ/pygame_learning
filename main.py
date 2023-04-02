9ed0a1a1b7d61dbcda91c0154e8539ab7a816ef6
krzysztofsztuk <krzysztof.jerzy.sztuk@gmail.com>
2023-04-02 19:30:01 +0200

    finished basic animations, starting rectangles

--- main.py
+++ main.py
@@ -6,14 +6,30 @@ if __name__ == '__main__':
 
     screen = pygame.display.set_mode((800, 400))
     pygame.display.set_caption("Runner")
+    clock = pygame.time.Clock()
+    test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
+
+    sky_surface = pygame.image.load("graphics/Sky.png").convert()
+    ground_surface = pygame.image.load("graphics/ground.png").convert()
+    text_surface = test_font.render("My game", False, "Black")
+
+    snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
+    snail_x_pos = 600
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-        # Draw all our elements
-        # Update everything
+
+        screen.blit(sky_surface, (0, 0))
+        screen.blit(ground_surface, (0, 300))
+        screen.blit(text_surface, (300, 50))
+        snail_x_pos -= 4
+        if snail_x_pos < -100: snail_x_pos = 800
+        screen.blit(snail_surface, (snail_x_pos, 250))
+
         pygame.display.update()
+        clock.tick(60)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
